2bce1079336dd5f3851e99eeb61ab17cf107cb68
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-09 13:26:47 -0400

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
